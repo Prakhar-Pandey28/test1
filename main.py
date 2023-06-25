6d526c1960234c70363491ef48c28eb0f947c587
prakharCSBS <prakhar.computersciencebusiness@gmail.com>
2023-06-26 00:37:02 +0530

    IIT-M_BS Data Science Python F2

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+Accept five words as input and print the sentence formed by these words after adding a space between consecutive words and a full stop at the end.
 word1 = input()
 word2 = input()
 word3 = input()
@@ -6,4 +7,4 @@ word5 = input()
 space = ' '
 stop = '.'
 sentence = word1+space+word2+space+word3+space+word4+space+word5+stop
-print(sentence) 
+print(sentence)
