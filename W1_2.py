48cba34f750f3515c8385293e59977ae3307cb5d
prakharCSBS <prakhar.computersciencebusiness@gmail.com>
2023-06-26 01:10:51 +0530

    IIT-M_BS Data Science Python F2

--- W1_2.py
+++ W1_2.py
@@ -1,4 +1,18 @@
 # Accept the date in DD-MM-YYYY format as input and print the year as output
-date = input()
-year = date[-4: ]
+date = input("Enter date: ")
+
+#year = date[0:10]
+# date[start: end] 28-11-2000
+# start = 0 index , 2 is starting in date and end 10 lastindex which is equal to 0.
+#output 28-11-2000
+
+year = date[-4:]
+
+#date[-4:]
+#[end:start] means starting from backside
+#output 2000
+
+#year = date[-4:-1]
+#output 200
+
 print(year)
