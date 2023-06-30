234409ea9c046544f4bce8c1d453a10d40c4ccb0
prakharCSBS <prakhar.computersciencebusiness@gmail.com>
2023-06-30 21:17:17 +0530

    IIT-M_BS Data Science Python F2

---
+++ W1_5.py
@@ -0,0 +1,9 @@
+#Accept two positive integers x and y as input.
+#Print the number of digits in x^y.
+#You should be able to solve this problem using the concepts covered in week-1.
+
+x = int(input())
+y = int(input())
+res = x**y
+res_str = str(res)
+print(len(res_str))
