b76ec3a8c195fdbcdd3e79a9928ce745cd163bcf
Prakhar Pandey <126502314+Prakhar-Pandey28@users.noreply.github.com>
2023-08-19 16:26:51 +0530

    Question- Two dictionaries D1 and D2 can be merged to create a new dictionary D that has the following structure: • Each key-value pair in D is present either in D1 or D2. • Each key in D1 is also a key in D. Likewise, each in D2 is also a key in D. • If a particular key is common to both D1 and D2, the value corresponding to this key in one of the two dictionaries is retained in D. Write a function named merge that accepts the following arguments: • D1: first dictionary • D2: second dictionary • priority: The is a string variable that denotes the priority given to common keys while merging. That is, if both D1 and D2 have a key in common, then this variable will determine which value needs to be retained. More specifically, priority can take one of these two values: • "first": retain the value corresponding to the common key present in the first dictionary • "second": retain the value corresponding to the common key present in the second dictionary This function should return the merged dictionary.

---
+++ mini_game.py
@@ -0,0 +1,15 @@
+command = ""
+while command.lower() !="quit":
+    command = input(">").lower()
+    if command== "start":
+        print("Chalu ho gayi.. chal di!")
+    elif command == "stop":
+        print("car band ho gayi")
+    elif command == "help":
+        print("""
+        Start - chalu karne ke liye
+        stop - band karne ke liye
+        quit - to quit
+        """)
+    else:
+        print("Sorry, itna padha likha nhi hu")
